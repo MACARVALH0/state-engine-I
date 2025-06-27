e9a4047c411043b25a8a919d0eecc146e65b2d69
MACARVALH0 <macarvalho.dev@gmail.com>
2025-06-27 16:04:07 -0300

    Aprimoramento na documentação de `TileMapLayer`.

--- engine/TileMapLayer.js
+++ engine/TileMapLayer.js
@@ -3,32 +3,58 @@ import TileSet  from './TileSet.js';
 
 export default class TileMapLayer
 {
-    constructor(width, height, cols, rows, tileset)
+    /**
+     * Construtor da classe `TileMapLayer`.
+     * 
+     * @param {*} width Largura do `TileMapLayer`.
+     * @param {*} height Altura `TileMapLayer`.
+     * @param {*} tileset Instância de objeto `ImageData` do atlas/tileset de sprites.
+     * @param  {...any} options Conjunto de configurações opcionais na construção do `TileMapLayer`.
+     */
+    constructor(width, height, tileset, ...options)
     {
+        /** Posição x do ponto de origem do `TileMapLayer` no canvas. */
         this.origin_x = 0;
+        
+        /** Posição y do ponto de origem do `TileMapLayer` no canvas. */
         this.origin_y = 0;
 
+
+        /** Largura do `TileMapLayer`. */
         this.width = Math.abs(width);
+
+        /** Altura do `TileMapLayer`. */
         this.height = Math.abs(height);
 
-        this.cols = parseInt(Math.abs(cols));
-        this.rows = parseInt(Math.abs(rows));
+
+        /** Largura de um tile. O programa pode assumir o valor 64 como padrão. */
+        this.tile_w = options['tile_w'] ? Math.abs(options['tile_w']) : 64;
+
+        /** Altura de um tile. O programa pode assumir o valor 64 como padrão. */
+        this.tile_h = options['tile_h'] ? Math.abs(options['tile_h']) : 64;
+
+
+        /** Número de colunas de tiles no `TileMapLayer`. */
+        this.cols = options['cols'] // Verifica se `cols` foi definido nas opções passadas como argumento para o construtor (caso existam).
+            ? Math.ceil(Math.abs(options['cols']))
+            : Math.ceil(this.width/this.tile_w);
+
+        /** Número de linhas de tiles no `TileMapLayer`. */
+        this.rows = options['rows'] // Verifica se `rows` foi definido nas opções passadas como argumento para o construtor (caso existam).
+            ? Math.ceil(Math.abs(options['cols']))
+            : Math.ceil(this.height/this.tile_h);
 
 
         /** Garante que a proporção entre os tiles seja mantida nas funções que alteram seus tamanhos.  */
-        this.keep_tile_ratio = true;
+        this.keep_tile_size_ratio = true;
 
-        /** Largura de um tile. O programa pode assumir o valor 32 como padrão. */
-        this.tile_w = Math.ceil(this.width/cols) || 32;
 
-        /** Altura de um tile. O programa pode assumir o valor 32 como padrão. */
-        this.tile_h = Math.ceil(this.height/rows) || 32;
 
         /** Matriz de tiles do objeto `TileMapLayer`. */
         this.tiles = this.setupTiles();
 
         /** Instância de TileSet do mapa. */
-        this.tileset = new TileSet(tileset ?? new ImageData(), this.tile_w, this.keep_tile_ratio ? this.tile_w : this.tile_h);
+        this.tileset = new TileSet(tileset ?? new ImageData(), this.tile_w, this.keep_tile_size_ratio ? this.tile_w : this.tile_h);
     }
 
 
