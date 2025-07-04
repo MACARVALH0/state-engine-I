0df15e708cd055ffbf5172863e1796949f1cdaa1
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-04 17:13:46 -0300

    Atualizando função de composição.

--- engine/utils/compose.js
+++ engine/utils/compose.js
@@ -1,11 +1,30 @@
-/** 
+/**
  * Função compositora de mixins em uma classe base.
- * 
- * @typedef {(base: any) => any} Mixin Uma função que recebe uma classe base e retorna uma classe estendida.
- *
- * @param {...Mixin} mixins Funções `mixin` cujas propriedades serão adicionadas à extensão da classe base.
- * @returns {(base: any) => any} Uma função que, ao receber uma classe base, retorna uma nova classe composta com os mixins.
+ * @param {*} Base Classe base para ser estendida.
+ * @param  {...mixins} mixins Funções que estendem uma determinada classe base.
+ * @returns Retorna uma classe composta com as propriedades dos mixins fornecidos.
  */
-const compose = (...mixins) => base => mixins.reduce( (acc, mixin) => mixin(acc), base );
+export default function compose(Base, ...mixins)
+{
+	const initial_routine = [];
 
-export default compose;
+	const Composed =  mixins.reduce( (acc, mixin) =>
+	{
+		const Mixin = mixin(acc);
+
+		// COMPOSIÇÃO DA ROTINA INICIAL DA CLASSE ESTENDIDA.
+		if(typeof Mixin.initialRoutine === 'function'){ initial_routine.push(Mixin.initialRoutine); }
+
+		return Mixin; // Retorna mixin para próxima iteração da função redutora.
+		
+	}, Base );
+
+	Composed.prototype.runInitialRoutine = function()
+	{
+		// console.log(`Running initial routine on "${this.name}"`);
+        for(let fn of initial_routine){ fn(); }
+		console.log(`O objeto "${this.name}" está adequado.`);
+	};
+
+	return Composed;
+}
