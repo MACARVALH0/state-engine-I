f70e6f87f54efa24440bce8ab2a370529e0ce8ff
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-04 17:13:11 -0300

    Atualizanção da classe.

--- engine/Keyboard.js
+++ engine/Keyboard.js
@@ -1,9 +1,16 @@
 import vkey from "./virtualKeyboardSetup.js";
+import EventManager from "./Observer/EventManager.js";
 
 const key_control = Object.entries(controls).vkey.map( ([key, _code_string]) =>{ return [key, false]; } )
 
 class Keyboard
 {
+    constructor()
+    {
+        this.eventManager = new EventManager();
+    }
+
+
     check(key_code){ return key_control.get(key_code); }
     
     update_key_state(key_code){ key_control.set(key_code, active); }
