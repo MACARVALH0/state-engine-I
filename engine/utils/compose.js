58e839d7ed9c12091bf6b3847a9930179c1f408a
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-11 17:27:48 -0300

    Adicionada função `composeEntity`.

--- engine/utils/compose.js
+++ engine/utils/compose.js
@@ -1,3 +1,5 @@
+import Entity from "../Entity";
+
 /**
  * Função compositora de mixins em uma classe base.
  * @param {*} Base Classe base para ser estendida.
@@ -19,9 +21,37 @@ export default function compose(Base, ...mixins)
 		
 	}, Base );
 
+	// Adiciona ao protótipo da classe composta uma função que executa as funções de rotina inicial existentes em cada mixin.
 	Composed.prototype.runInitialRoutine = function()
 	{
-		// console.log(`Running initial routine on "${this.name}"`);
+		console.log(`Running initial routine on "${this.name}"`); // FIXME Debug, não será necessário adiante.
+        for(let fn of initial_routine){ fn(); }
+		console.log(`O objeto "${this.name}" está adequado.`);
+	};
+
+	return Composed;
+}
+
+
+export function composeEntity(...mixins)
+{
+	const initial_routine = [];
+
+	const Composed =  mixins.reduce( (acc, mixin) =>
+	{
+		const Mixin = mixin(acc);
+
+		// COMPOSIÇÃO DA ROTINA INICIAL DA CLASSE ESTENDIDA.
+		if(typeof Mixin.initialRoutine === 'function'){ initial_routine.push(Mixin.initialRoutine); }
+
+		return Mixin; // Retorna mixin para próxima iteração da função redutora.
+		
+	}, Entity );
+
+	// Adiciona ao protótipo da classe composta uma função que executa as funções de rotina inicial existentes em cada mixin.
+	Composed.prototype.runInitialRoutine = function()
+	{
+		console.log(`Running initial routine on "${this.name}"`); // FIXME Debug, não será necessário adiante.
         for(let fn of initial_routine){ fn(); }
 		console.log(`O objeto "${this.name}" está adequado.`);
 	};
