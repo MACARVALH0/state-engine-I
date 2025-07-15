0a9daff5784deed522aa0ffc470be9a03d49b928
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-15 18:10:03 -0300

    Adição: - composeGeneric
    mudanças menores.

--- engine/utils/compose.js
+++ engine/utils/compose.js
@@ -38,6 +38,39 @@ export function compose(Base, ...mixins)
 }
 
 
+export function composeGeneric(...mixins)
+{
+	// Lista de funções de rotina inicial dos mixins.
+	const initial_routine = [];
+
+	const mixin_list = [...mixins];
+	mixin_list.push(Observer); // Propriedade de Observer.
+
+	const Composed =  mixin_list.reduce( (acc, mixin) =>
+	{
+		const Mixin = mixin(acc);
+
+		// COMPOSIÇÃO DA ROTINA INICIAL DA CLASSE ESTENDIDA.
+		if(typeof Mixin.initialRoutine === 'function'){ initial_routine.push(Mixin.initialRoutine); }
+
+		return Mixin; // Retorna mixin para próxima iteração da função redutora.
+		
+	}, class Generic {} );
+
+	// Adiciona ao protótipo da classe composta uma função que executa as funções de rotina inicial existentes em cada mixin.
+	Composed.prototype.runInitialRoutine = function()
+	{
+		console.log(`Running initial routine on "${this.name}"`); // FIXME Debug, não será necessário adiante.
+		initial_routine.forEach( fn => fn() );
+		console.log(`O objeto "${this.name}" está adequado.`);
+	};
+
+	return Composed;
+}
+
+
+
+
 /**
  * Função compositora de mixinscom base na classe `Entity`.
  * @param  {...mixins} mixins Funções que estendem a classe base.
@@ -45,9 +78,10 @@ export function compose(Base, ...mixins)
  */
 export function composeEntity(...mixins)
 {
-	// Lista de funções de rotina inicial dos mixins.
+	/** Lista de funções de rotina inicial dos mixins. */
 	const initial_routine = [];
 
+	/** Lista de mixins na qual são adicionados outros valores arbitrários. */
 	const mixin_list = [...mixins];
 	mixin_list.push(Observer); // Propriedade de Observer.
 
