109612a8a8976d557bec7212eb0bc75ee462f872
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-15 18:14:56 -0300

    Criação da classe.

---
+++ engine/Observer/Observer.js
@@ -0,0 +1,45 @@
+// TODO Documentar classe.
+const Observer = Base => class extends Base
+{
+    constructor(...config)
+    {
+        super(...config);
+        
+        /** Mapa no modelo {tipo_de_evento, handler}. */
+
+        /**
+         * Mapa no modelo {tipo_de_evento, handler}.
+         * @example
+         * this.handlers = new Map
+         * ([
+         *      ["scene_created", (e) => {}],
+         *      ["scene_deleted", (e) => {}]
+         * ]);
+         */
+        this.handlers = new Map();
+    }
+
+    /**
+     * Registra um handler de evento no Observer.
+     * @param {String} event_type Tipo de evento.
+     * @param {Function} callback Função handler para o tipo de evento.
+     */
+    createHandler(event_type, callback)
+    {
+        this.handlers.set(event_type, callback);
+    }
+
+
+    /**
+     * Função responsável por redirecionar cada tipo de notificação de evento disparada ao seu handler específico.
+     * @param {*} event_type Tipo de evento.
+     * @param {*} data Dados enviados para os handlers de evento.
+     */
+    handleEvent(event_type, data)
+    { 
+        if(this.handlers?.has(event_type)){ this.handlers.get(event_type)(data); }
+        else { throw new Error(`O handler para eventos do tipo ${event_type} não existe.`); }
+    }
+};
+
+export default Observer;
