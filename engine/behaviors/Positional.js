7b42fb1a044887afbe75345f32e2a6c36a5e07d4
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-26 12:01:21 -0300

    Mudanças em diretórios.

---
+++ engine/behaviors/Positional.js
@@ -0,0 +1,27 @@
+import Transform from "../components/Transform.js";
+import { composeBehavior } from "../utils/compose.js"; 
+
+/**
+ * Uma extensão de classe para objetos que agregam propriedades transformacionais num plano bidimensional,
+ * como posições horizontal e vertical, valores de scaling, rotação, etc.
+ * 
+ * @param {*} Base Classe base que acumulará as propriedades de `Positional`.
+ */
+const Positional = Base => 
+{
+    // TODO Atualizar a documentação da classe.
+
+    const Composition = composeBehavior(Base, Transform);
+
+    return class extends Composition
+    {
+        constructor(...config)
+        {
+            // Construtor do behavior `Positional`.
+            super(...config);
+            console.log("- Positional");
+        }
+    }
+};
+
+export default Positional;
