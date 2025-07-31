5821941dc2698aa027d55d66fc47a8d8b6e3199b
MACARVALH0 <macarvalho.dev@gmail.com>
2025-07-31 17:07:32 -0300

    Criação do renderer.

---
+++ engine/renderer/CanvasRenderer.js
@@ -0,0 +1,27 @@
+export default class CanvasRenderer
+{
+    /**
+     * Construtor do `CanvasRenderer`.
+     * @param {CanvasRenderingContext2D} context Interface do `CanvasRenderingContext2D`, da API do Canvas para a Web.
+     */
+    constructor(context)
+    {
+        this.ctx = context;
+    }
+
+    /**
+     * Retorna o objeto `ImageData` do canvas, desde o `CanvasRenderer`.
+     * @param {Number} x Posição `x` do canvas.
+     * @param {Number} y Posição `y`do canvas.
+     * @param {Number} w Quantidade de pixels representando a largura desde a posição `x`.
+     * @param {Number} h Quantidade de pixels representando a altura desde a posição `x`.
+     * @returns {ImageData} Interface que representa os dados subjacentes dos pixels de uma área do elemento `<canvas>`.
+     */
+    getImageData(x, y, w, h){ return this.ctx.getImageData(x, y, w, h); }
+
+    // TODO Documentar método.
+    drawSprite(texture, x, y)
+    {
+        this.ctx.drawImage(texture, x, y);
+    }
+}
