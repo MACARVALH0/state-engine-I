42fb1635d879dd7d5350be10734a3c1ea8357441
MACARVALH0 <macarvalho.dev@gmail.com>
2025-08-07 17:39:41 -0300

    Removido: - import de `vkey`; - `ctx` localmente.
    Adição:
    - `last_update`;
    - método `setupKeyboard`
    
    Mudanças:
    - Novo parâmetro em `createScene`;
    - Nova relação com função de atualização.

--- engine/Game.js
+++ engine/Game.js
@@ -1,6 +1,5 @@
 import SceneManager from "./SceneManager.js";
 import Scene        from "./Scene.js";
-import vkey         from "./virtualKeyboardSetup.js";
 
 import Observer     from "./Observer/Observer.js";
 import Publisher    from "./Observer/Publisher.js";
@@ -23,19 +22,29 @@ export default class Game extends game_composition
         // Tenta definir as propriedades básicas de largura, altura e contexto.
         try
         {
+            // Cache do canvas na instância de `Game`.
+            this.canvas = canvas;
+
             /** Largura do canvas. */
-            this.canvas_width = canvas.width;
+            this.canvas_width = this.canvas.width;
 
             /** Altura do canvas. */
-            this.canvas_height = canvas.height;
+            this.canvas_height = this.canvas.height;
 
-            /** Contexto do canvas. */
-            this.ctx = canvas.getContext('2d');
+            // OBSOLETO
+            // /** Contexto do canvas. */
+            // this.ctx = canvas.getContext('2d');
 
         } catch(e){ throw new Error(`O argumento do construtor deve ser um elemento \`canvas\` HTML.\n${e}`); }
 
         /** Gerenciador de cenas do jogo. */
         this.sceneManager = new SceneManager();
+
+        /**
+         * Período transpassado desde o carregamento do contexto de `window`.
+         * Serve para o cálculo de tempo a cada atualização no contexto.
+         */
+        this.last_update = undefined;
 
         /** ID do laço de execuçãos. */
         this.loop_id = undefined;
@@ -49,8 +58,9 @@ export default class Game extends game_composition
      * 
      * @param {Number} width Opcional. Valor de largura customizada para a nova cena. Caso não esteja presente, assume a largura do canvas.
      * @param {Number} height Opcional. Valor de altura customizada para a nova cena. Caso não esteja presente, assume a altura do canvas.
+     * @param {Object} options Objeto de configurações relacionadas à instância de `Scene` que será criada.
      */
-    createScene(width, height)
+    createScene(width, height, options)
     {
         const MAX_SCENE_NUMBER = 16;
         if(this.sceneManager.stack.length > MAX_SCENE_NUMBER) throw new Error("O número máximo de cenas foi excedido.");
@@ -60,10 +70,18 @@ export default class Game extends game_composition
         (
             width ?? this.canvas_width,
             height ?? this.canvas_height,
-            this.ctx
+            this.canvas,
+            options ?? {}
         );
 
-        scene.eventManager.subscribe(this, "keyboard_req"); // `Game` se torna um observer de eventos da instância de `Scene`.
+        /*
+            Espera notificação de:
+            - Nova entidade criada;
+        */
+        scene.eventManager.subscribe(this, "entity_created");
+        
+        // OBSOLETO
+        // scene.eventManager.subscribe(this, "keyboard_req"); // `Game` se torna um observer de eventos da instância de `Scene`.
 
         this.sceneManager.push(scene); // Adiciona a cena ao `SceneManager` de `Game`.
 
@@ -71,45 +89,42 @@ export default class Game extends game_composition
     }
 
 
+    /** Inicia o loop principal da instância de `Game`. */
     start()
     {
-        let last_time = performance.now();
+        this.last_update = performance.now();
 
-        /**
-         * Função callback que será executada em loop e armazenada como atributo na instância de game, para possibilidade futura de pausa.
-         * É uma *arrow function*, para manter o contexto de `Game`.
-         * 
-         * Cada vez que `requestAnimationFrame` chama `gameLoop`, aquela passa como argumento o atual tempo de execução do programa, o que é importante aqui.
-         * 
-         * @param {Number} current_time Tempo de execução do programa, passado por `requestAnimationFrame`.
-         */
-        const gameLoop = (current_time) =>
-        {
-            const delta = (current_time - last_time) / 1000; // Tempo que passou, em segundos.
-            last_time = current_time;
-
-            // ROTINA PRINCIPAL //
-
-            this.update(delta);
-
-            // // // // // // //
-
-            this.loop_id = requestAnimationFrame(gameLoop); // Invoca a próxima execução de gameLoop.
-        }
-
-        requestAnimationFrame(gameLoop); // Invoca a primeira execução de `gameLoop`;
+        this.update = this.update.bind(this);
+        requestAnimationFrame(this.update);
     }
 
-    update(delta)
+    /**
+     * Função callback que será executada em loop e armazenada como atributo na instância de game, para possibilidade futura de pausa.
+     * É uma *arrow function*, para manter o contexto de `Game`.
+     * 
+     * Cada vez que `requestAnimationFrame` chama `gameLoop`, aquela passa como argumento o atual tempo de execução do programa, o que é importante aqui.
+     * 
+     * @param {Number} current_time Tempo de execução do programa, passado por `requestAnimationFrame`.
+     */
+    update(current_time)
     {
-        this.sceneManager.update(delta);
+        const delta = (current_time - this.last_update) / 1000; // Tempo que passou, em segundos.
+        this.last_update = current_time;
+
+        // // // ROTINA PRINCIPAL // // //
+
+            // Atualizar cenas do `sceneManager`.
+            this.sceneManager.update(delta);
+
+        // // // // // // // // // // // //
+
+        this.loop_id = requestAnimationFrame(this.update); // Invoca a próxima execução de gameLoop.
     }
 
-    
-    /** OBSOLETE */
-    setupKeyboard()
-    {
-        if(this.keyboard_control) return;
-        this.keyboard_control = Object.entries(controls).vkey.map( ([key, _code_string]) =>{ return [key, false]; } )
-    }
+    /* OBSOLETO */
+    // setupKeyboard()
+    // {
+    //     if(this.keyboard_control) return;
+    //     this.keyboard_control = Object.entries(controls).vkey.map( ([key, _code_string]) =>{ return [key, false]; } )
+    // }
 }
