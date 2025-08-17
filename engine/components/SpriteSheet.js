ea696e3f6264e404e4203e671bf5aa9a11b4aa41
MACARVALH0 <macarvalho.dev@gmail.com>
2025-08-17 13:26:37 -0300

    Criação. Ainda em elaboração de conceito.

---
+++ engine/components/SpriteSheet.js
@@ -0,0 +1,14 @@
+// TODO Documentar classe.
+const SpriteSheet = Base => class extends Base
+{
+    constructor(...config)
+    {
+        super(...config);
+
+        this.sprites = new Map();
+    }
+
+    // TODO Documentar método.
+    defineSprites()
+    {}
+}
