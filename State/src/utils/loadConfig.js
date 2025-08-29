ed964b992da3ed6d173b960c5924e948e5f5f115
MACARVALH0 <macarvalho.dev@gmail.com>
2025-08-29 15:09:11 -0300

    Criação do utilitário de carrega configurações.

--- State/src/utils/loadConfig.js
+++ State/src/utils/loadConfig.js
@@ -1,19 +1,24 @@
 import fs from "fs";
 import yaml from "js-yaml";
 
-export function loadConfig(file_path = "./config.yml")
+export function loadDirConfig(file_path = "./State/src/dir_config.yml")
 {
-    try
-    {
+	try
+	{
         const file = fs.readFileSync(file_path, "utf8");
-        const config = yaml.load(file);
+
+		/**
+		 * Conteúdo do arquivo de configuração de caminhos para diretórios de arquivos do jogo.
+		 * @type {Object} Objeto de configurações.
+		 */
+    	const config = yaml.load(file);
       
         return config;
-      }
-      catch (err)
-      {
-        console.error("Erro ao carregar YAML:", err);
-        throw new Error("Não foi possível carregar o arquivo YAML:\n"+err);
-      }
-      
+    }
+
+    catch (err)
+	{
+		// console.error("Erro ao carregar YAML:", err);
+		throw new Error("Não foi possível carregar o arquivo YAML em \`" + file_path + "\`.\n"  + err);
+    }
 }
