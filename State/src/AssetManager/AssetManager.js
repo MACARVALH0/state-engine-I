94da94406de981d7771bddfb153fa4eb62a1d687
MACARVALH0 <macarvalho.dev@gmail.com>
2025-08-30 18:06:13 -0300

    Alterações conceituais.

--- State/src/AssetManager/AssetManager.js
+++ State/src/AssetManager/AssetManager.js
@@ -7,8 +7,27 @@ const asset_manager_composition = composeGeneric(ImageManager);
 // const AssetManager = Base => class extends Base
 export default class AssetManager extends asset_manager_composition
 {
-    constructor(base_path = 'public/')
+    constructor(assets_path = "Game/Assets", ...config)
     {
-        super();
+        super(...config);
+
+        this.base_path = assets_path;
+
+        /** Caminhos para os arquivos dos assets separados por tipo. */
+        this.files = this.getPaths();
+    }
+
+    /** Captura informações de Assets via `manifest.json` no caminho especificado em `this.assets_path`. */
+    async getPaths()
+    {
+        const route = this.base_path + "/manifest.json";
+
+        const response = await fetch(route);
+        if(!response.ok) throw new Error( "Não foi possível carregar informações de assets.\n"+err );
+
+        const content = await response.json();
+        console.log("Arquivo de caminhos de sprites carregado.")
+
+        return content;
     }
 };
