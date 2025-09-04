e39b8bf598bf6a5bc0489a09bc5a8c40dd0a8714
MACARVALH0 <macarvalho.dev@gmail.com>
2025-09-03 21:40:36 -0300

    Criação do índice.

---
+++ State/src/behaviors/index.js
@@ -0,0 +1,7 @@
+export { default as Animatable          }   from "./Animatable.js";
+export { default as Cacheable           }   from "./Cacheable.js";
+export { default as Controllable        }   from "./Controllable.js";
+export { default as Movable             }   from "./Movable.js";
+export { default as PhysicalObject2D    }   from "./PhysicalObject2D.js";
+export { default as Positional          }   from "./Positional.js";
+export { default as Visible             }   from "./Visible.js";
