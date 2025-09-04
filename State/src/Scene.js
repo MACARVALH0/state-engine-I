f7ce59fb40f80813118f0255782e9ea77cf90607
MACARVALH0 <macarvalho.dev@gmail.com>
2025-09-04 14:40:04 -0300

    Novos métodos; Pequenas atualizações.

--- State/src/Scene.js
+++ State/src/Scene.js
@@ -5,7 +5,11 @@ import RenderSystem     from './systems/RenderSystem.js';
 
 import Observer         from './Observer/Observer.js';
 import Publisher        from './Observer/Publisher.js';
-import { composeGeneric } from "./utils/compose.js";
+import { composeEntity, composeGeneric } from "./utils/compose.js";
+import Shape from './components/Shape.js';
+import Entity from './Entity.js';
+
+import * as behaviors from "./behaviors/behaviors.js"
 
 const scene_composition = composeGeneric(Observer, Publisher);
 
@@ -23,6 +27,8 @@ export default class Scene extends scene_composition
     {
         super();
 
+        this.entity_ids = new Set();
+        
         /** Conjunto de entidades/objetos presentes na cena atual. */
         this.entities = [];
 
@@ -42,11 +48,13 @@ export default class Scene extends scene_composition
     }
 
 
+
     /**
      * Adiciona uma entidade composta ao conjunto de elementos no objeto do jogo.
      * @param {String} name Nome referenciável do objeto.
      * @param {ComposedClass} Composition Composição de classe base + mixins de propriedades.
      * @param {...any} config Itens de configuração das propriedades dos mixins.
+     * @returns {Entity} Entidade adicionada à cena.
      */
     addComposedEntity(name, Composition, ...config)
     {
@@ -55,6 +63,7 @@ export default class Scene extends scene_composition
             // Criar um objeto com a composição fornecida.
             const entity = new Composition(name, config);
 
+            // DEPRECATED Isso vai contra o padrão ECS...
             // Executa a rotina inicial do objeto composto (caso exista) e verifica se ele está adequado.
             entity.runInitialRoutine();
 
@@ -74,6 +83,75 @@ export default class Scene extends scene_composition
             else console.error("Não foi possível criar o objeto composto:\n", err);
         }
     };
+
+
+    addComposedEntity1(name, ...config)
+    {
+        if(this.entity_ids.has(name)){ throw new Error("Já existe uma entidade com esse identificador."); }
+
+        // FIXME Isso aqui obviamente precisa de uma melhorada.
+        const { Animatable, Cacheable, Controllable, Movable, PhysicalObject2D, Positional, Visible } = behaviors;
+
+        // Top repetições dos animes...
+        const entity_composition = composeEntity( Animatable, Cacheable, Controllable, Movable, PhysicalObject2D, Positional, Visible );
+
+        const entity = new entity_composition(name, ...config);
+
+        this.entity_ids.add(name);
+
+        // Atualmente sem utilidade.
+        // this.entities.push(entity);
+
+        // Verifica o pertencimento da entidade nos sistemas registrados.
+        for(let [_, system] of this.systems){ system.register(entity); }
+
+        return entity;
+    }
+
+
+    // FIXME Provisório.
+    /**
+     * Cria uma polígono com dado número de lados.
+     * @param {*} n_sides Número de lados do polígono.
+     * @param  {...any} options
+     * @returns {Shape} Uma referência para o objeto que representa a forma. 
+     */
+    createShapeEntity(n_sides, ...options)
+    {
+        const TWO_PI = 2*Math.PI;
+        const angle = TWO_PI / Math.round(n_sides);
+
+        const r = options["r"] ?? 32;
+
+        const points = [];
+
+        const width =   options["width"]  ?? r;
+        const height =  options["height"] ?? r;
+
+        for(let i = 1; i < n_sides; i++)
+        {
+            const a = angle*i;
+
+            const x = angle + width  * Math.cos(a);
+            const y = angle + height * Math.sin(a);
+
+            // Adiciona o par ordenado do ponto à array de pontos.
+            points.push([x, y]);
+        }
+
+        const entity_composition = composeEntity(Shape);
+
+        const entity = new entity_composition(points);
+
+        // Adiciona objeto ao conjunto de entidades presentes na cena.
+        // FIXME Isso aqui precisa ser revisto.
+        this.entities.push(entity);
+
+        // FIXME Talvez pensar numa forma de não iterar por todos os sistemas a fim de registrar a entidade.
+        for(let [_, system] of this.systems){ system.register(entity); }
+
+        return entity;
+    }
 
 
     update(delta)
