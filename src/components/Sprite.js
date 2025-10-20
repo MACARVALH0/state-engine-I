3e7c8dc124ed196badb0587afd8870d388fc2aba
Matheus <macarvalho.dev@gmail.com>
2025-10-20 10:41:29 -0300

    `Sprite` agora é uma view, não um componente.

--- src/components/Sprite.js
+++ src/components/Sprite.js
@@ -1,10 +1,26 @@
 // TODO Documentar classe.
-const Sprite = Base => class extends Base
+// const Sprite = Base => class extends Base
+// {
+//     constructor(...config)
+//     {
+//         super(...config);
+
+//         /** Sobrecarrega o tipo de renderização da entidade para "Sprite". */
+//         this.kind = "sprite";
+
+//         /** Textura/Imagem do sprite. */
+//         this.texture = config["texture_src"] ?? undefined;
+//     }
+// }
+
+class Sprite
 {
     constructor(...config)
     {
-        super(...config);
+        /** Sobrecarrega o tipo de renderização da entidade para "Sprite". */
+        this.view_type = "sprite";
 
+        /** Textura/Imagem do sprite. */
         this.texture = config["texture_src"] ?? undefined;
     }
 }
