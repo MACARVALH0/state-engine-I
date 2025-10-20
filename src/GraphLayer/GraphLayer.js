75b0d7eade7a8560464b0df82a19ec7bfa14a9f6
Matheus <macarvalho.dev@gmail.com>
2025-10-20 10:42:09 -0300

    Início da abstração.

---
+++ src/GraphLayer/GraphLayer.js
@@ -0,0 +1,24 @@
+export default class GraphLayer
+{
+    constructor()
+    {
+    
+    }
+}
+
+
+// /**
+//  * @template T
+//  * @param {*} Base 
+//  * @returns {new (...args: any[]) => T}
+//  */
+// const GraphLayer = Base => class extends Base
+// {
+//     constructor()
+//     {
+//         super();
+//     }
+// }
+
+// export default GraphLayer;
+
