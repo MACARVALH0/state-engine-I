5b42b01f2c356a3c263157c76670d3f95b3eabd3
Matheus <macarvalho.dev@gmail.com>
2025-10-20 10:30:31 -0300

    Mudanças: - `Entity` mudou de mixin -> classe. - A classe não estende mais uma composição local.
    Removido:
    - Trava de segurança para `name`.
    - Atributo `this.components`.
    - Método `has()`, aprofundando o padrão ECS.

--- src/Entity.js
+++ src/Entity.js
@@ -1,15 +1,5 @@
-import Observer     from "./Observer/Observer.js";
-import Publisher    from "./Observer/Publisher.js";
-
-import { composeGeneric } from "./utils/compose.js";
-
-
-const entity_composition = composeGeneric(Observer, Publisher);
-
-/**
- * Classe base para instanciação de uma entidade na engine.
- */
-export default class Entity extends entity_composition
+// TODO Documentar classe.
+class Entity
 {
     /**
      * Construtor da classe básica.
@@ -17,21 +7,15 @@ export default class Entity extends entity_composition
      */
     constructor(name)
     {
-        // if(!name) throw new Error("É necessário dar um nome ao objeto.");
-        console.log(`Criando o objeto "${name}"...`);
+        console.log(`\nCriando o objeto "${name}"...`);
         console.log("Classe de entidade básica, com as seguintes extensões:");
 
-        super();
         
         this.name = name;
         this.initial_routine_functions = [];
 
-        this.components = new Map();
-
         console.log("Nova `Entity` criada.");
     }
-
-    // TODO Documentar método.
-    has(...props){ return props.every(p => p in this); }
-
 }
+
+export default Entity;
