4fdc5f5961dbd15d640d66487b09afc211dfa4d3
Matheus <macarvalho.dev@gmail.com>
2025-10-20 10:41:19 -0300

    `Shape` agora é uma view, não mais um componente.

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,25 +1,23 @@
-const Shape = Base => class extends Base
+class Shape
 {
-    constructor()
+    constructor(config)
     {
-        /** Posição x do centro da forma. */
-        this.x = 0;
+        // super(...config);
 
-        /** Posição y do centro da forma. */
-        this.y = 0;
+        // Sobrecarrega o tipo de renderização da entidade para "Shape".
+        this.view_type = "shape";
 
         /** Cor de preenchimento do polígono. */
-        this.color = "";
+        this.color = config["color"] ?? "rgb(255, 250, 250)";
 
         /** Cor das bordas do polígono. */
-        this.b_color = "";
+        this.border_color = config["border_color"] ?? "rgb(30, 30, 30)";
 
+        this.points = config["points"] ?? [];
         /** Conjunto de pontos representando os vértices do polígono. */
-        this.points = new Map( points.map( ([x, y], i) => [ `p${i}`, [x, y] ] ) );
-
-        console.log( this.points ); // DEBUG
-            
+        // this.points = new Map( points.map( ([x, y], i) => [ `p${i}`, [x, y] ] ) );
     }
 }
+
 
 export default Shape;
