6be1ea332797c61dad6ee019185f27b2556252c7
Matheus <macarvalho.dev@gmail.com>
2025-10-24 17:09:35 -0300

    Algumas mudanças na classe.

--- src/Entity.js
+++ src/Entity.js
@@ -9,9 +9,10 @@ class Entity
     {
         console.log(`\nCriando o objeto "${name}"...`);
         console.log("Classe de entidade básica, com as seguintes extensões:");
-
         
         this.name = name;
+        this.components = new Set();
+
         this.initial_routine_functions = [];
 
         console.log("Nova `Entity` criada.");
