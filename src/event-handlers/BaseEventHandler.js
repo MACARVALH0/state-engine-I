2251d147020fb60b799b92e9c8189c517fa7d718
Matheus <macarvalho.dev@gmail.com>
2025-10-29 09:11:22 -0300

    Mudança no nome do diretório.

--- src/event-handlers/BaseEventHandler.js
+++
@@ -1,8 +0,0 @@
-// TODO Documentar classe baseEventHandler.
-export default class baseEventHandler
-{
-    constructor()
-    {
-        console.log("Preparado para lidar com eventos básicos.");
-    }
-}
